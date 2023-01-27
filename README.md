278771adfb97415a2ee2ffbc866b6cdcf84e726e
Vytas Stuopis <vytas.stuopis@gmail.com>
2023-01-26 22:10:22 -0500

    Adding in my index.html and corresponding data for mapping multiple charts for the Belly Button Biodiversity project.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Belly-Button-Biodiversity
+
+## Background
+Looking at a variety of test subjects we collected data to find the most common bacteria in the subjects bellybutton. We mapped the top 10 Cultures found in a hotizontal bar chart while including the belly button washing frequency in a gauge chart. We proceeded to use a bubble chart at the bottom of the page to determine the bacteria cultures per sameple.
---
+++ charts.js
@@ -0,0 +1,159 @@
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+// Initialize the dashboard
+init();
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildMetadata(newSample);
+  buildCharts(newSample);
+  
+}
+
+// Demographics Panel 
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+   
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
+
+// Deliverable 1: 1. Create the buildChart function.
+function buildCharts(sample) {
+  // Deliverable 1: 2. Use d3.json to load the samples.json file 
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+
+    // Deliverable 1: 3. Create a variable that holds the samples array. 
+  var samplesArray= data.samples;
+    // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
+  var desiredSampleNumber= samplesArray.filter(sampleObj => sampleObj.id == sample);
+
+    // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
+  var metadataArray= data.metadata.filter(sampleObj => sampleObj.id == sample);
+    // Deliverable 1: 5. Create a variable that holds the first sample in the array.
+  var firstSample= desiredSampleNumber[0];
+    // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
+  var metadataSample1= metadataArray[0];
+    // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+  var otu_ids= firstSample.otu_ids;
+  var otu_labels= firstSample.otu_labels;
+  var sample_values= firstSample.sample_values;
+    // Deliverable 3: 3. Create a variable that holds the washing frequency.
+  var wash_frequency= metadataSample1.wfreq;
+
+    // Deliverable 1: 7. Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order 
+    // so the otu_ids with the most bacteria are last. 
+  var yticks = otu_ids.slice(0,10).map(outId =>`OTU ${outId}`).reverse();
+    // Deliverable 1: 8. Create the trace for the bar chart. 
+  var barTrace = { 
+        x: sample_values.slice(0,10).reverse(),
+        y: yticks,
+        type: "bar",
+        orientation:'h'    
+  };
+  
+  var barData = [barTrace];
+  
+    // Deliverable 1: 9. Create the layout for the bar chart. 
+    var barLayout = {
+        title: "Top 10 Bacteria Cultures Found",
+        indexAxis: 'y'
+  };
+    // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
+  Plotly.newPlot("bar", barData, barLayout)
+    // Deliverable 2: 1. Create the trace for the bubble chart.
+  var bubbleTrace = {
+    x: otu_ids,
+    y: sample_values,
+    text: otu_labels,
+    mode: 'markers',
+    marker: {
+      color: otu_ids,
+      size: sample_values
+    }
+  };
+
+  var bubbleData = [bubbleTrace];
+    // Deliverable 2: 2. Create the layout for the bubble chart.
+  var bubbleLayout = {
+    title: 'Bacteria Cultures Per Sample',
+    xAxisID: 'OTU ID',
+    margin: 1,
+    hovermode: 'closest' 
+  }
+
+    // Deliverable 2: 3. Use Plotly to plot the data with the layout.
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+    // Deliverable 3: 4. Create the trace for the gauge chart.
+  var gaugeTrace = 
+  {
+    domain: {x: [0,1], y: [0,1] },
+    type: "indicator",
+    mode: "gauge+number",
+    value: wash_frequency,
+    title: {text: "Belly Button Washing Frequency"},
+    gauge: {
+      axis: { range: [null, 10] },
+      bar: {color: "black"},
+      bgcolor: "white",
+      borderwidth: 1,
+      bordercolor: "gray",
+      steps: [
+        { range: [0,2], color: "salmon" },
+        { range: [2,4], color: "sienna" },
+        { range: [4,6], color: "olivedrab" },
+        { range: [6,8], color: "lavender" },
+        { range: [8,10], color: "hotpink" },
+      ],
+    }
+  }
+  console.log(wash_frequency)
+  var gaugeData = [gaugeTrace];
+    // Deliverable 3: 5. Create the layout for the gauge chart.
+  var gagueLayout = {
+    width: 500,
+    heigh: 400
+  };
+    // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
+  Plotly.newPlot("gauge", gaugeData, gagueLayout)
+  //, gagueLayout)
+  });
+}
